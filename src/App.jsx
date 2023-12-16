3cc663e483265cf3206e753f3dec44a779be4fa2
Danyal Imran <dan.imran97@gmail.com>
2023-12-16 15:53:01 -0500

    initializing first commit for kanban-app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className='text-3xl text-center'>
+      
+      <Header />
+    </div>
+  )
+}
+
+export default App
