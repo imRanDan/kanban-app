1789ac6d709b05a1bb53f9ee27952ebee24d60c2
Danyal Imran <dan.imran97@gmail.com>
2023-12-16 18:04:20 -0500

    adding in more info for boards

--- src/components/HeaderDropdown.jsx
+++ src/components/HeaderDropdown.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import boardIcon from '../assets/icon-board.svg'
 
 function HeaderDropdown(setOpenDropdown) {
+
+  const boards = useSelector((state) => state.boards)
+
+
+
   return (
     <div
     className='py-10 px-6 absolute left-0 right-0 bottom-[-100vh] top-16
@@ -22,12 +29,21 @@ function HeaderDropdown(setOpenDropdown) {
       >
 
         <h3
-        className='dark:text-gray-300 text-gray-600 font-semibold mx-4 mb-8'
+        className='dark:text-gray-300 text-gray-600 font-semibold mx-4 text-left mb-8'
         >
-          All boards
+          All boards ({boards?.length})
         </h3>
 
         <div>
+          {boards.map((board, index) => (
+            <div
+            className={`flex items-baseline space-x-2 px-5 py-4 ${board.isActive && 'bg-[#635fc7] rounded-r-full text-white mr-8'}`}
+            key={index}
+            >
+              <img></img>
+
+            </div>
+          ))}
 
         </div>
 
